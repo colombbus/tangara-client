c5d805ffecb9f0cec84d2fe8de9f17b7b69a3a8f
bonpra <benoit.prady@colombbus.org>
2014-04-09 19:26:13 +0200

    Function to get keycode from char

--- js/tangara/TUtils.js
+++ js/tangara/TUtils.js
@@ -96,6 +96,17 @@ define(['jquery'], function($) {
             }
         }
 
+        var keyCodes = {
+        "backspace":0, "tab":9, "return":13, "shift":16, "ctrl":17, "alt":18, "pausebreak":19, "capslock":20, "escape":27, " ":32, "pageup":33,
+        "pagedown":34, "end":35, "home":36, "left":37, "up":38, "right":39, "down":40, "+":[43,107], "printscreen":44, "insert":45, "delete":46,
+        "0":[48,96], "1":[49,97], "2":[50,98], "3":[51,99], "4":[52,100], "5":[53,101], "6":[54,102], "7":[55,103], "8":[56,104], "9":[57,105], ";":[59,186],
+        "=":[61,187], "a":65, "b":66, "c":67, "d":68, "e":69, "f":70, "g":71, "h":72, "i":73, "j":74, "k":75, "l":76,
+        "m":77, "n":78, "o":79, "p":80, "q":81, "r":82, "s":83, "t":84, "u":85, "v":86, "w":87, "x":88, "y":89, "z":90,
+        "*":106, "-":[109,189], ".":[110,190], "/":[111,191],
+        "f1":112, "f2":113, "f3":114, "f4":115, "f5":116, "f6":117, "f7":118, "f8":119, "f9":120, "f10":121, "f11":122, "f12":123,
+        "numlock":144, "scrolllock":145, ",":188, "`":192, "[":219, "\\":220, "]":221, "'":222
+        };
+
         this.removeAccents = function(str) {
             var letters = str.split("");
             var newStr = "";
@@ -113,12 +124,22 @@ define(['jquery'], function($) {
         };
         
         this.checkInteger = function(value) {
-	        return (typeof value !== 'undefined' && !isNaN(value));
+            return (typeof value !== 'undefined' && !isNaN(value));
         };
         
         this.checkString = function(value) {
-	        return (typeof value !== 'undefined' && (typeof value === 'string' || value instanceof String));
+            return (typeof value !== 'undefined' && (typeof value === 'string' || value instanceof String));
         };
+        
+        this.getkeyCode = function(value) {
+            if (this.checkString(value)) {
+                if (typeof keyCodes[value] !== 'undefined') {
+                    return keyCodes[value];
+                }
+            }
+            return false;
+        };
+        
     };
     
     var utilInstance = new TUtils();
