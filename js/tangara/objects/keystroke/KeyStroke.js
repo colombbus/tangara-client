8de86716de9ca7d708b981aeba4ab740ec1e5c5b
bonpra <benoit.prady@colombbus.org>
2014-04-15 18:42:48 +0200

    Rewrite KeyStroke object

--- js/tangara/objects/keystroke/KeyStroke.js
+++ js/tangara/objects/keystroke/KeyStroke.js
@@ -2,6 +2,21 @@ define(['jquery','TEnvironment', 'TUtils', 'CommandManager', 'objects/TObject'],
     var KeyStroke = function() {
         TObject.call(this);
         this.commands = new CommandManager();
+        this.active = true;
+        this.keyDown = false;
+        this.keyboardEnabled = false;
+        this.checkAllKeysUp = false;
+        this.keys = new Array();
+        var that = this;
+        this.listenerKeyDown = function(e) {
+            that.processKeyDown(e);
+            e.preventDefault();
+        };
+        this.listenerKeyUp = function(e) {
+            that.processKeyUp(e);
+            e.preventDefault();
+        };
+        this.enableKeyboard();
     };
 
     var qInstance = TEnvironment.getQuintusInstance();
@@ -10,65 +25,148 @@ define(['jquery','TEnvironment', 'TUtils', 'CommandManager', 'objects/TObject'],
     KeyStroke.prototype.constructor = KeyStroke;
     KeyStroke.prototype.className = "KeyStroke";
 
+    KeyStroke.prototype.getKeyCode = function(key) {
+        key = TUtils.removeAccents(key);
+        key = this.getMessage(key);
+        return TUtils.getkeyCode(key);
+    };
+
+    KeyStroke.prototype.enableKeyboard = function() {
+        if (this.keyboardEnabled) {
+            return false;
+        }
+        var element = qInstance.el;
+
+        // Copied from Quintus_input
+        element.tabIndex = 0;
+        element.style.outline = 0;
+        
+        element.addEventListener("keydown",this.listenerKeyDown, false);
+        element.addEventListener("keyup",this.listenerKeyUp, false);
+
+        this.keyboardEnabled = true;
+    };
+
     KeyStroke.prototype._addCommand = function(key, command) {
         if (TUtils.checkString(key)&&TUtils.checkCommand(command)) {
-            key = TUtils.removeAccents(key);
-            key = this.getMessage(key);
-            var keycode = TUtils.getkeyCode(key);
+            var keycode = this.getKeyCode(key);
             if (keycode !== false) {
-                var eventName = "key_"+keycode;
-                if (!this.commands.hasCommands(eventName)) {
-                    // add key binding for this key
-                    var map = new Array();
-                    map[keycode] = eventName;
-                    qInstance.input.keyboardControls(map);
-                    qInstance.input.on(eventName, this, "processKey");
-                    qInstance.input.on(eventName+"Up", this, "processKeyUp");
-                }
-                this.commands.addCommand(command, eventName);
+                this.keys[keycode] = false;
+                this.commands.addCommand(command, keycode+"_down");
             }
         }
     };
     
     KeyStroke.prototype._removeCommands = function(key) {
         if (TUtils.checkString(key)) {
-            key = TUtils.removeAccents(key);
-            key = this.getMessage(key);
-            var keycode = TUtils.getkeyCode(key);
+            var keycode = this.getKeyCode(key);
             if (keycode !== false) {
-                var eventName = "key_"+keycode;
-                if (this.commands.hasCommands(eventName)) {
-                    qInstance.input.keys[keycode] = undefined;
-                    this.commands.removeCommands(eventName);
+                this.commands.removeCommands(keycode+"_down");
+                if (! this.commands.hasCommands(keycode+"up")) {
+                    this.keys[keycode] = undefined;
                 }
             }
         }
     };
+
+    KeyStroke.prototype._addCommandRelease = function(param1, param2) {
+        var key, command;
+        if (typeof param2 !== 'undefined') {
+            key = param1;
+            command = param2;
+        } else {
+            command = param1;
+        }
+        if (TUtils.checkCommand(command)) {
+            if (TUtils.checkString(key)) {
+                // command to be launched when a given key is released
+                var keycode = this.getKeyCode(key);
+                if (keycode !== false) {
+                    this.keys[keycode] = false;
+                    this.commands.addCommand(command, keycode+"_up");
+                }
+            } else {
+                // command to be launched when all keys are released
+                this.commands.addCommand(command, "key_up_all");
+                this.checkAllKeysUp = true;
+            }
+        }
+    };
+
+    KeyStroke.prototype._removeCommandRelease = function(key) {
+        if (TUtils.checkString(key)) {
+            // remove commands to be launched when a given key is released
+            var keycode = this.getKeyCode(key);
+            if (keycode !== false) {
+                this.commands.removeCommands(keycode+"_up");
+                if (! this.commands.hasCommands(keycode+"down")) {
+                    this.keys[keycode] = undefined;
+                }
+            }
+        } else {
+            // remove commands to be launched when all keys are released
+            this.commands.removeCommands("key_up_all");
+            this.checkAllKeysUp = false;
+        }
+    };
     
     KeyStroke.prototype._activate = function() {
-	    
+        this.active = true;
     };
 
     KeyStroke.prototype._deactivate = function() {
-	    
+        if (this.active) {
+            this.active = false;
+            for (var keycode in this.keys) {
+                this.keys[keycode] = false;
+            }
+        }
     };
     
     KeyStroke.prototype.deleteObject = function() {
-    	
-	    TObject.deleteObject.call(this);
-    };
+        // remove listeners
+        var element = qInstance.el;
+        element.removeEventListener("keydown",this.listenerKeyDown, false);
+        element.removeEventListener("keyup",this.listenerKeyUp, false);
 
-    KeyStroke.prototype.processKey = function() {
-        var that = this;
-        $.each(qInstance.inputs, function(eventName, value) {
-            if (value) {
-                that.commands.executeCommands({'field':eventName});
-            }
-        });
-    };
-
-    KeyStroke.prototype.processKeyUp = function() {
+        // delete commands
+        for (var keycode in this.keys) {
+            this.commands.removeCommands(keycode+"_down");
+            this.commands.removeCommands(keycode+"_up");
+        }
+            this.commands.removeCommands("key_up_all");
+        this.commands = undefined;
         
+        // delete keys
+        this.keys.length = 0;
+        this.keys = undefined;
+        
+	TObject.deleteObject.call(this);
+    };
+
+    KeyStroke.prototype.processKeyDown = function(e) {
+        if (this.active) {
+            var keycode = e.keyCode;
+            this.commands.executeCommands({'field':keycode+"_down"});
+            this.keys[keycode] = true;
+        }
+    };
+
+    KeyStroke.prototype.processKeyUp = function(e) {
+        if (this.active) {
+            var keycode = e.keyCode;
+            this.commands.executeCommands({'field':keycode+"_up"});
+            this.keys[keycode] = false;
+            if (this.checkAllKeysUp) {
+                for (var value in this.keys) {
+                    if (this.keys[value]) {
+                        // there is still a key down: we skip the test
+                        return;
+                    }
+                }
+                this.commands.executeCommands({'field':"key_up_all"});
+            }
+        }
     };
 
     TEnvironment.internationalize(KeyStroke);
