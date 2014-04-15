5a4cbdf5f7cc01c900ebc74f9e1128ac94227688
bonpra <benoit.prady@colombbus.org>
2014-04-15 12:36:12 +0200

    Added ability to execute a command given as a function, with an optional parameter

--- js/tangara/sandbox.js
+++ js/tangara/sandbox.js
@@ -10,14 +10,22 @@ require.config({
 require(['jquery'],function($) {
     $(document).ready( function() {
         window.thatObject = Object.create(null);
-        window.execute = function(commands) {
+        window.execute = function(commands, parameter) {
             // sandbox : prevent window and document objects from direct access 
             // and remove reference to 'this'
             // not secure though : these objects can still be accessed using functions
             // e.g. (function(){this.window.alert("hello");})()
-            var objectsSandbox = new Function('window', 'document', commands);
-            var thisSandbox  = objectsSandbox.bind(window.thatObject);
-            thisSandbox({}, {});
+            var objectsSandbox, thisSandbox;
+            if (typeof commands === 'string' || commands instanceof String) {
+                // commands are string: we wrap them in a function
+                objectsSandbox = new Function('window', 'document', commands);
+                var thisSandbox  = objectsSandbox.bind(window.thatObject);
+                thisSandbox({}, {});
+            } else if (typeof commands === 'function'  || commands instanceof Function) {
+                // commands are function: we cannot neutralize window and document objects
+                thisSandbox  = commands.bind(window.thatObject);
+                thisSandbox(parameter);
+            }
         };
         parent.bindSandbox(window.execute);
     });
