afd84d9f6dd6c801a09d6145c9e4455cc82d9ce8
bonpra <benoit@192.168.0.15>
2014-05-26 22:19:29 +0200

    Make TProgram unaware of editor

--- js/tangara/TProgram.js
+++ js/tangara/TProgram.js
@@ -1,48 +1,24 @@
 define(['TParser'], function(TParser) {
 
     function TProgram() {
-        var saved = false;
-        var dirty = true;
-        var editor;
         var statements;
         var code = "";
         
         
-        this.setDirty = function(value) {
-            dirty = value;
-            if (dirty) {
-                saved = false;
-            }
-        };
-
         this.save = function() {
             // TODO: save file
-            saved = true;
-        };
-
-        
-        this.setCode = function(value) {
-            code = value;
-        };
-        
-        function updateCode() {
-            code = editor.getValue();
-        };
-        
-        this.setEditor = function(value) {
-            editor = value;
         };
         
         function parse() {
             statements = TParser.parse(code);
         }
-        
+
+        this.setCode = function(value) {
+            code = value;
+            parse();
+        };
+       
         this.getStatements = function() {
-            if (dirty) {
-                updateCode();
-                parse();
-                dirty = false;
-            }
             return statements;
         };
     }
