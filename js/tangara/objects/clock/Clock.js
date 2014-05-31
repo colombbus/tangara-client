cd2ecce6a0aea4937def44cf1f832232317bdf44
bonpra <benoit@192.168.0.15>
2014-05-31 16:05:48 +0200

    Added object Clock

---
+++ js/tangara/objects/clock/Clock.js
@@ -0,0 +1,87 @@
+define(['jquery','TEnvironment', 'TObject', 'TUtils', 'CommandManager'], function($, TEnvironment, TObject, TUtils, CommandManager) {
+    var Clock = function() {
+        TObject.call(this);
+        this.commands = new CommandManager();
+        this.delay = 1000;
+        this.initialDelay = 0;
+        this.running = false;
+        this.timeout = null;
+        this.loop = true;
+    };
+    
+    Clock.prototype = Object.create(TObject.prototype);
+    Clock.prototype.constructor = Clock;
+    Clock.prototype.className = "Clock";    
+    
+    Clock.prototype._addCommand = function(command) {
+        if (TUtils.checkCommand(command)) {
+            this.commands.addCommand(command);
+        } else {
+            throw new Error(this.getMessage("wrong command"));
+        }
+    };
+
+    Clock.prototype._removeCommands = function() {
+        this.commands.removeCommands();
+    };
+
+    Clock.prototype._setDelay = function(delay) {
+        if (TUtils.checkInteger(delay)) {
+            this.delay = delay;
+        } else {
+            throw new Error(this.getMessage("wrong delay"));
+        }
+    };
+
+    Clock.prototype._setInitialDelay = function(delay) {
+        if (TUtils.checkInteger(delay)) {
+            this.initialDelay = delay;
+        } else {
+            throw new Error(this.getMessage("wrong delay"));
+        }
+    };
+
+    Clock.prototype._start = function() {
+        if (!this.running) {
+            this.running = true;
+            var self = this;
+            this.timeout = window.setTimeout(function() { self.executeActions(); }, this.initialDelay);
+        }
+    };
+
+    Clock.prototype._stop = function() {
+        this.running = false;
+        if (this.timeout !== null) {
+            window.clearTimeout(this.timeout);
+            this.timeout = null;
+        }
+    };
+    
+    Clock.prototype.executeActions = function() {
+        this.timeout = null;
+        if (this.running) {
+            this.commands.executeCommands();
+            if (this.loop) {
+                var self = this;
+                this.timeout = window.setTimeout(function() { self.executeActions(); }, this.delay);
+            }
+        }
+    };
+
+    Clock.prototype._delete = function() {
+        this.stop();
+        TObject.prototype._delete.call(this);
+    };
+
+    Clock.prototype._loop = function(value) {
+        if (TUtils.checkBoolean(value)) {
+            this.loop = value;
+        }
+    };
+    
+    TEnvironment.internationalize(Clock, true);
+    
+    return Clock;
+});
+
+
