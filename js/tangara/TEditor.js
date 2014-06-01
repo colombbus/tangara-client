5b0cdb337587660520adef93435db46e057facdc
bonpra <benoit@192.168.0.15>
2014-06-01 22:30:00 +0200

    Multiple files management: sidebar, close/open/new programs, keep track of modifications

--- js/tangara/TEditor.js
+++ js/tangara/TEditor.js
@@ -1,4 +1,4 @@
-define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvironment'], function($,ace, ace_edit_session, ace_range, TProgram, TEnvironment) {
+define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvironment', 'TLink'], function($,ace, ace_edit_session, ace_range, TProgram, TEnvironment, TLink) {
 
     function TEditor() {
         var domEditor = document.createElement("div");
@@ -6,18 +6,19 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvir
         // start with editor hidden
         domEditor.style.display="none";
         
+        var domEditorSidebar = document.createElement("div");
+        domEditorSidebar.id = "teditor-sidebar";
+        domEditor.appendChild(domEditorSidebar);
+        
         var domEditorText = document.createElement("div");
         domEditorText.id = "teditor-text";
         domEditor.appendChild(domEditorText);
 
         
         var aceEditor;
-        var dirty = false;
         var codeChanged = false;
-        var programs = new Array();
-        var sessions = new Array();
+        var editing = new Array();
         var currentProgram = null;
-        var newIndex = 1;
         var AceEditSession = ace_edit_session.EditSession;
         var AceRange = ace_range.Range;
         var errorMarker = null;
@@ -34,6 +35,10 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvir
             aceEditor.setHighlightActiveLine(false);
             var self = this;
             aceEditor.on('input', function() {
+                if (!currentProgram.isModified()) {
+                    currentProgram.setModified(true);
+                    self.updateProgramName();
+                }
                 codeChanged = true;
                 self.removeError();
                 /*if (!aceEditor.getSession().getUndoManager().hasUndo())
@@ -41,6 +46,14 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvir
                 else
                     dirty = true;*/
             });
+            aceEditor.commands.addCommand({
+                name: "save",
+                bindKey: {win: "Ctrl-S", mac: "Command-S"},
+                exec: function(arg) {
+                    self.saveProgram();
+                }
+            });
+            
             this.newProgram();
             /* NOTE FOR LATER
             $('#save').on("click", function() {
@@ -75,49 +88,91 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvir
         };
         
         this.editProgram = function(name) {
-            if (name !== currentProgram.getName()) {
-                if (typeof sessions[name] !== 'undefined') {
+            if (currentProgram === null || name !== currentProgram.getName()) {
+                if (typeof editing[name] !== 'undefined') {
                     // program already opened
                     saveSession();
                     openSession(name);
+                    this.updateSidebar();
                 } else {
                     // program not already opened : we open it
                     this.openProgram(name);
+                    this.updateSidebar();
                 }
             }
+            aceEditor.focus();            
         };
         
         function saveSession() {
             if (currentProgram !== null) {
-                sessions[currentProgram.getName()] = aceEditor.getSession();
+                editing[currentProgram.getName()]['session'] = aceEditor.getSession();
+                currentProgram.setCode(aceEditor.getSession().getValue());
+                codeChanged = false;
             }
         }
         
         function openSession(name) {
-            aceEditor.setSession(sessions[name]);
-            currentProgram = programs[name];
+            aceEditor.setSession(editing[name]['session']);
+            currentProgram = editing[name]['program'];
         }
 
-        function createSession(name) {
-            var program = new TProgram(name);
-            programs.push(program);
-            currentProgram = program;
+        function createSession(program) {
             var session = new AceEditSession(program.getCode());
             session.setMode("ace/mode/java");
-            sessions.push(session);
+            editing[program.getName()]['session'] = session;
             aceEditor.setSession(session);
         }
         
         this.openProgram = function(name) {
             saveSession();
-            createSession(name);
+            var program = new TProgram(name);
+            currentProgram = program;
+            editing[name] = new Array();
+            editing[name]['program'] = program;
+            createSession(program);
         };
         
         this.newProgram = function() {
-            var name = TEnvironment.getMessage('program-new',newIndex);
             saveSession();
-            createSession(name);
-            newIndex++;
+            var program = new TProgram();
+            editing[program.getName()] = new Array();
+            editing[program.getName()]['program'] = program;
+            currentProgram = program;
+            createSession(program);
+            this.updateSidebar();
+            aceEditor.focus();
+        };
+        
+        this.closeProgram = function(name) {
+            var editedPrograms = Object.keys(editing);
+            editedPrograms = sortArray(editedPrograms);
+            var deletedIndex = editedPrograms.indexOf(name);
+            if (currentProgram.isModified()) {
+                var goOn = window.confirm(TEnvironment.getMessage('close-confirm', name));
+                if (!goOn) {
+                    aceEditor.focus();
+                    return;
+                }
+            }
+            delete editing[name];
+            currentProgram = null;
+            // Find program to display
+            if (deletedIndex > 0) {
+                this.editProgram(editedPrograms[deletedIndex-1]);
+            } else if (editedPrograms.length>1) {
+                // deleted was first: display following
+                this.editProgram(editedPrograms[1]);
+            } else {
+                // there are no more edited program: create one
+                this.newProgram();
+            }
+            aceEditor.focus();
+        };
+        
+        this.saveProgram = function() {
+            saveSession();
+            currentProgram.save();
+            this.updateProgramName();
         };
         
         this.removeError = function() {
@@ -139,6 +194,67 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'TProgram', 'TEnvir
             aceEditor.navigateTo(lines[0]-1, 0);
             // In a timer, because otherwise does not seem to work when editor mode has just been activated
             setTimeout(function() { aceEditor.scrollToLine(lines[0]-1, true, true, null); }, 100);
+        };
+        
+        this.updateSidebar = function() {
+            var programList = TLink.getProgramList();
+            var editedPrograms = Object.keys(editing);
+            
+            // Sort programs alphabetically
+            programList = sortArray(programList);
+            editedPrograms = sortArray(editedPrograms);
+            
+            domEditorSidebar.innerHTML = "";
+
+
+            function addElement(editor, name, displayedName, edited, current) {
+                var element = document.createElement("div");
+                element.className = "teditor-sidebar-program";
+                if (edited) {
+                    element.className += " teditor-sidebar-edited";
+                }
+                if (typeof current !== 'undefined' && current) {
+                    element.className += " teditor-sidebar-current";
+                }
+                element.onclick = function() { editor.editProgram(name);};
+                var nameElement = document.createElement("div");
+                nameElement.id = "teditor-sidebar-program-"+TProgram.findId(name);
+                nameElement.appendChild(document.createTextNode(displayedName));
+                element.appendChild(nameElement);
+                if (edited) {
+                    var closeElement = document.createElement("div");
+                    closeElement.className = "teditor-sidebar-close";
+                    closeElement.onclick = function(e) { editor.closeProgram(name);e.stopPropagation();};
+                    element.appendChild(closeElement);
+                }
+                domEditorSidebar.appendChild(element);
+            }
+
+            var currentName = "";
+            
+            if (typeof currentProgram !== 'undefined') {
+                currentName = currentProgram.getName();
+            }
+
+            for (var i=0; i<editedPrograms.length;i++) {
+                addElement(this, editedPrograms[i], editing[editedPrograms[i]]['program'].getDisplayedName(), true, editedPrograms[i] === currentName);
+            }
+            
+            for (var i=0; i<programList.length;i++) {
+                var name = programList[i];
+                if (typeof editing[name] === 'undefined') {
+                    addElement(this, name, name, false);
+                }
+            }
+        };
+        
+        function sortArray(value) {
+            return value.sort(function (a, b) { return a.toLowerCase().localeCompare(b.toLowerCase());});
+        }
+        
+        this.updateProgramName = function() {
+            var id = "#teditor-sidebar-program-"+currentProgram.getId();
+            $(id).text(currentProgram.getDisplayedName());
         };
         
     };
