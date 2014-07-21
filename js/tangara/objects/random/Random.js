5cb884d07358d3bed8331daf609b1b4e146ee9ec
bonpra <benoit.prady@colombbus.org>
2014-07-21 11:45:05 +0200

    Added Random object

---
+++ js/tangara/objects/random/Random.js
@@ -0,0 +1,17 @@
+define(['TEnvironment', 'TObject', 'TUtils', 'TRuntime'], function(TEnvironment, TObject, TUtils) {
+    var Random = function() {
+        TObject.call(this);
+    };
+    
+    Random.prototype = Object.create(TObject.prototype);
+    Random.prototype.constructor = Random;
+    Random.prototype.className = "Random";
+    
+    Random.prototype._throwDice = function(max) {
+        return Math.floor((Math.random() * max) + 1); 
+    };
+
+    TEnvironment.internationalize(Random, true);
+    
+    return Random;
+});
