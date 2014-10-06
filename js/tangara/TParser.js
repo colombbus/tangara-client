08fefd6bb4412b4014a145fc02cc9ad09c50050d
bonpra <benoit.prady@colombbus.org>
2014-10-06 15:14:53 +0200

    Added ability to protect Tangara object names

--- js/tangara/TParser.js
+++ js/tangara/TParser.js
@@ -1,11 +1,22 @@
 define(['acorn'], function(acorn) {
     function TParser() {
         var options = {locations:true, forbidReserved:"everywhere"};
+        
+        var globalReserved = ['window', 'document', 'eval'];
+        
+        acorn.addReservedWords(globalReserved);
+        
+        this.protectIdentifiers = function(names) {
+            acorn.addReservedIdentifiers(names);
+        };
+        
         this.parse = function(input) {
             var result = acorn.parse(input, options);
             // return statements
             return result.body;
         };
+        
+        
     }
     
     var parserInstance = new TParser();
