0ffa8e35a26358afdd428531cf17f1e8baf43b69
bonpra <benoit.prady@colombbus.org>
2014-10-21 20:52:17 +0200

    Added image editor

--- js/tangara/TViewer.js
+++ js/tangara/TViewer.js
@@ -1,6 +1,9 @@
-define(['TUI', 'TEnvironment', 'jquery'], function(TUI, TEnvironment, $) {
+define(['TUI', 'TEnvironment', 'jquery', 'wColorPicker', 'wPaint', 'wPaint/plugins/main','wPaint/plugins/text', 'wPaint/plugins/shapes', 'wPaint/plugins/file'], function(TUI, TEnvironment, $) {
     function TViewer() {
         var currentName = '';
+        var currentWidth =0;
+        var currentHeight =0;
+        
         var nextHandler = null;
         var prevHandler = null;
 
@@ -10,13 +13,20 @@ define(['TUI', 'TEnvironment', 'jquery'], function(TUI, TEnvironment, $) {
         domImage.className= "tviewer-image";
         var image = document.createElement("img");
         image.onload = function() {
-                domImage.appendChild(image);
-                domMain.appendChild(domImage);
+                $domImage.show();
                 imageDisplayed = true;
                 $domMain.removeClass("loading");
                 updateImageSize();
+                currentWidth = image.naturalWidth;
+                currentHeight = image.naturalHeight;
+                $domTitle.text(currentName+" ("+currentWidth+"x"+currentHeight+")");
         };
         domImage.appendChild(image);
+        var domButtonEdit = document.createElement("div");
+        domButtonEdit.className = "tviewer-button-edit";
+        domButtonEdit.onclick = function() {
+            edit();
+        };
         var domButtonClose = document.createElement("div");
         domButtonClose.className = "tviewer-button-close";
         domButtonClose.onclick = function() {
@@ -38,16 +48,63 @@ define(['TUI', 'TEnvironment', 'jquery'], function(TUI, TEnvironment, $) {
         };
         var domTitle = document.createElement("div");
         domTitle.className = "tviewer-title";
+        domImage.appendChild(domButtonEdit);
         domImage.appendChild(domButtonClose);
         domImage.appendChild(domButtonLeft);
         domImage.appendChild(domButtonRight);
         domImage.appendChild(domTitle);
+        domMain.appendChild(domImage);
+        
+        var domEditor = document.createElement("div");
+        domEditor.className = "tviewer-editor";
+
+        var domEditorImage = document.createElement("div");
+        domEditorImage.className = "tviewer-editor-image";
+        
+        var domButtonClose2 = document.createElement("div");
+        domButtonClose2.className = "tviewer-button-close";
+        domButtonClose2.onclick = function() {
+            hide();
+        };
+        
+        var domMessage = document.createElement("div");
+        domMessage.className = "tviewer-editor-message";
+        
+        domEditor.appendChild(domEditorImage);
+        domEditor.appendChild(domButtonClose2);
+        domEditor.appendChild(domMessage);
+        domMain.appendChild(domEditor);
         
         var $domMain = $(domMain);
         var $domTitle = $(domTitle);
         var $domImage = $(domImage);
         var $image = $(image);
-        $domMain.hide();        
+        var $domEditor = $(domEditor);
+        var $domEditorImage = $(domEditorImage);
+        var $domMessage = $(domMessage);
+        
+        $domMain.hide();
+        $domImage.hide();
+        $domEditor.hide();
+        $domMessage.hide();
+        
+        var editorInitialized = false;
+        
+        // remove load buttons from wPaint menu
+        delete $.fn.wPaint.menus.main.items.loadBg;
+        delete $.fn.wPaint.menus.main.items.loadFg;
+        
+        $.extend($.fn.wPaint.defaults, {
+            saveImg: function() {
+                var imageData = $domEditorImage.wPaint("image");
+                try {
+                    TEnvironment.getProject().setResourceContent(currentName, imageData);
+                    message(TEnvironment.getMessage('image-editor-saved', currentName));
+                } catch (error) {
+                    message(error.getMessage());
+                }
+           }
+        });
         
         var keyHandler = function(event) {
             switch(event.which){
@@ -71,23 +128,36 @@ define(['TUI', 'TEnvironment', 'jquery'], function(TUI, TEnvironment, $) {
         
         var appended = false;
         var imageDisplayed = false;
+        var imageEdited = false;
         
         var hide = function() {
-            if (appended) {
-                $domMain.fadeOut();       
-                document.body.removeChild(domMain);
-                $(window).off('keydown', keyHandler);
-                appended = false;
+            if (imageEdited) {
+                // was in editing mode: get back to display mode
+                $domEditor.hide();
+                $domImage.show();
+                imageEdited = false;
+                imageDisplayed = true;
+                // refreshImage
+                image.src = TEnvironment.getProjectResource(currentName)+"?" + new Date().getTime();
+                //displayImage(currentName);
+            } else {
+                if (appended) {
+                    $domMain.fadeOut();
+                    document.body.removeChild(domMain);
+                    $(window).off('keydown', keyHandler);
+                    appended = false;
+                }
+                if (imageDisplayed) {
+                    $domImage.hide();
+                    imageDisplayed = false;
+                }
             }
-            if (imageDisplayed) {
-                domMain.removeChild(domImage);
-                imageDisplayed = false;
-            }            
         };
         
         var displayImage = function(name) {
             if (imageDisplayed) {
-                domMain.removeChild(domImage);
+                $domImage.hide();
+                //domMain.removeChild(domImage);
                 imageDisplayed = false;
             }
             $domMain.addClass("loading");
@@ -96,16 +166,42 @@ define(['TUI', 'TEnvironment', 'jquery'], function(TUI, TEnvironment, $) {
             $image.css("max-width","");
             $image.css("max-height","");
             image.src = TEnvironment.getProjectResource(name);
-            if (image.complete) {
-                // image already loaded
-                image.onload();
-            }
-            $domTitle.text(name);
+            $domTitle.text("");
         };
         
         var updateImageSize = function() {
             $image.css("max-width", $domImage.width());
             $image.css("max-height", $domImage.height());
+        };
+        
+        var edit = function() {
+            $domImage.hide();
+            $domEditor.show();
+            $domEditorImage.width(currentWidth);
+            $domEditorImage.height(currentHeight);
+            $domEditorImage.css("margin-left","-"+Math.round(currentWidth/2)+"px");
+            $domEditorImage.css("margin-top","-"+Math.round(currentHeight/2)+"px");
+            if (!editorInitialized) {
+                $domEditorImage.wPaint({
+                    path: TEnvironment.getBaseUrl()+TEnvironment.getConfig('wpaint-path'),
+                    image:TEnvironment.getProjectResource(currentName)
+                });
+                editorInitialized = true;
+            } else {
+                $domEditorImage.wPaint('clear');
+                $domEditorImage.wPaint('resize');
+                $domEditorImage.wPaint('image', TEnvironment.getProjectResource(currentName));
+            }
+            imageDisplayed = false;
+            imageEdited = true;
+        };
+        
+        var message = function(text) {
+            $domMessage.text(text).show().delay(2000).fadeOut();
+        };
+        
+        this.displayed = function() {
+            
         };
         
         this.setName = function(value) {
