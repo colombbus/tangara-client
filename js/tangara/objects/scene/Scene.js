25671c11b240f5e484d794c255dc9b22b6543086
bonpra <benoit@mobile-06.pn.colombbus.org>
2014-11-05 23:37:17 +0100

    Enhanced Scene

--- js/tangara/objects/scene/Scene.js
+++ js/tangara/objects/scene/Scene.js
@@ -16,7 +16,28 @@ define(['jquery','TEnvironment', 'TGraphicalObject', 'objects/sprite/Sprite', 'o
     
     var qInstance = Scene.prototype.qInstance;
     
-    Scene.prototype.qSprite = qInstance.TBlock;
+    qInstance.TBlock.extend("TScene", {
+        init: function(props,defaultProps) {
+            this._super(qInstance._extend({
+                computing:false
+            },props),defaultProps);
+        },
+        draw: function(ctx) {
+            var p = this.p;
+            if (!p.computing) {
+                this._super(ctx);
+            }
+        },
+        setComputing: function() {
+            this.p.computing = true;
+        },
+        unsetComputing: function() {
+            this.p.computing = false;
+        }
+
+    });
+    
+    Scene.prototype.qSprite = qInstance.TScene;
     
     
     Scene.prototype.setDisplayedImage = function(name) {
@@ -28,7 +49,11 @@ define(['jquery','TEnvironment', 'TGraphicalObject', 'objects/sprite/Sprite', 'o
                 if (!this.displayBlock) {
                     // display background
                     this.setDisplayedImage(this.backgroundName);
+                } else {
+                    this.qObject.unsetComputing();
                 }
+            } else {
+                this.qObject.unsetComputing();                    
             }
         }
     };
@@ -61,6 +86,7 @@ define(['jquery','TEnvironment', 'TGraphicalObject', 'objects/sprite/Sprite', 'o
                 // set initialized to false, to be sure that location will be set after next image is displayed
                 // (and width and height are correctly set)
                 parent.qObject.p.initialized = false;
+                parent.qObject.setComputing();
                 parent.setDisplayedImage(parent.blockName);
                 if (currentLocation !== false) {
                     parent.qObject.setLocation(currentLocation.x, currentLocation.y);
@@ -81,6 +107,30 @@ define(['jquery','TEnvironment', 'TGraphicalObject', 'objects/sprite/Sprite', 'o
         this.setDisplayedImage(this.backgroundName);
     };
 
+    Scene.prototype._setBackground = function(name) {
+        name = TUtils.getString(name);
+        try {
+            this.removeImage(this.backgroundName);
+        } catch (e) {}
+        this.backgroundName = name;
+        this.addImage(this.backgroundName, "elements", true);
+        if (!this.displayBlock) {
+            this.setDisplayedImage(this.backgroundName);
+        }
+    };
+
+    Scene.prototype._setBlock = function(name) {
+        name = TUtils.getString(name);
+        try {
+            this.removeImage(this.blockName);
+        } catch (e) {}
+        this.blockName = name;
+        this.addImage(this.blockName, "elements", true);
+        // anyway, because we have to re-compute mask
+        this.qObject.setComputing();
+        this.setDisplayedImage(this.blockName);
+    };    
+    
     TEnvironment.internationalize(Scene, true);
     
     return Scene;
