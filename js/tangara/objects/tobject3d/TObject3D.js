eb203a9925bfd99197ddac47d34eb5eb6c394a6c
Neonunux <regis.leloup@colombbus.org>
2014-11-25 19:39:06 +0100

    TObject3D creation: properties and 'delete' method defined

--- js/tangara/objects/tobject3d/TObject3D.js
+++ js/tangara/objects/tobject3d/TObject3D.js
@@ -3,16 +3,39 @@ define(['jquery', 'babylon', 'TEnvironment', 'TObject'], function($, babylon, TE
         TObject.call(this);
     };
 
-    var mesh = new BABYLON.Mesh;
+    var mesh = BABYLON.Mesh;
+    var object3d;
+    var scene;
     
-    TObject3D.prototype.objectName;
+    TObject3D.prototype = Object.create(TObject.prototype);
+    TObject3D.prototype.constructor = TObject3D;
     TObject3D.prototype.className = "TObject3D";
 
     TObject3D.prototype.getMesh = function (){
-        return mesh;
+        return this.mesh;
     };
+    
+    TObject3D.prototype.getObject3D = function (){
+        return this.object3d;
+    };
+    
+    TObject3D.prototype.setObject3D = function (thisObject){
+        this.object3d = thisObject;
+    };
+    
     TObject3D.prototype.freeze = function(value) {
         // every object may add actions to take to freeze
+    };
+    TObject3D.prototype._delete = function() {
+        this.object3d.dispose();
+    };
+    
+    /**
+     * 
+     * @param Space3D space
+     */
+    TObject3D.prototype.setSpace = function(scene3d){
+        this.scene = scene3d;
     };
     
     TObject3D.prototype.toString = function() {
