67edd884f402c48018a9df7287a9c6cee855b8b2
Neonunux <regis.leloup@colombbus.org>
2014-11-25 19:41:36 +0100

    sphere3D tweaks

--- js/tangara/objects/3dsphere/Sphere3D.js
+++ js/tangara/objects/3dsphere/Sphere3D.js
@@ -1,17 +1,25 @@
-define(['jquery', 'babylon', 'TEnvironment', 'TUtils', 'TObject', 'CommandManager'], function($, babylon, TEnvironment, TUtils, TObject, CommandManager) {
-    var Sphere3D = function(space3d) {
-        var scene = space3d.getScene();
-        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-        matos = new BABYLON.StandardMaterial("texture1", scene);
-        matos.diffuseColor = new BABYLON.Color3.Red();
-        sphere.material = matos;
-        sphere.position.y = 1;
+define(['jquery', 'babylon', 'TEnvironment', 'TObject3D'], function($, babylon, TEnvironment, TObject3D) {
+    /**
+     * 
+     * @returns Sphere3D
+     */
+    var Sphere3D = function() {
+        
+    };
+    var object3d;
+    var scene;
+
+    Sphere3D.prototype = Object.create(TObject3D.prototype);
+    Sphere3D.prototype.constructor = TObject3D;
+    Sphere3D.prototype.className = "Sphere3D";
+    
+    Sphere3D.prototype.setSpace = function(scene3d){
+        TObject3D.prototype.setSpace.call(this, scene3d);
+        scene = scene3d;
+        object3d = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+        object3d.position.y = 1;
     };
     
-    Sphere3D.prototype = Object.create(TObject.prototype);
-    Sphere3D.prototype.constructor = TObject;
-    Sphere3D.prototype.className = "Sphere3D";
-
     TEnvironment.internationalize(Sphere3D, true);
 
     return Sphere3D;
