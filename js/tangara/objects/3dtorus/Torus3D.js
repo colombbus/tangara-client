8701c1087c4cf3d3a5ae79288f8036b6a019b7ee
Neonunux <regis.leloup@colombbus.org>
2014-12-03 15:48:21 +0100

    Torus3D constructor cleaning and addSpace implement

--- js/tangara/objects/3dtorus/Torus3D.js
+++ js/tangara/objects/3dtorus/Torus3D.js
@@ -1,12 +1,15 @@
-define(['jquery', 'babylon', 'TEnvironment', 'TObject'], function($, babylon, TEnvironment, TObject) {
+define(['jquery', 'babylon', 'TEnvironment', 'TObject3D'], function($, babylon, TEnvironment, TObject3D) {
     var Torus3D = function() {
-        var cylinder = BABYLON.Mesh.CreateTorus("torus1", 6, 1, 36, scene, true);
-        cylinder.position.y = 1;
     };
-    Torus3D.prototype = Object.create(TObject.prototype);
+    Torus3D.prototype = Object.create(TObject3D.prototype);
     Torus3D.prototype.constructor = Torus3D;
     Torus3D.prototype.className = "Torus3D";
-
+    
+    Torus3D.prototype._setSpace = function(scene3d) {
+        TObject3D.prototype._setSpace.call(this, scene3d);
+        this.object3d = this.getMesh().CreateTorus("torus1", 6, 1, 36, this.scene, true);
+        this.object3d.position.y = 1;
+    };
     TEnvironment.internationalize(Torus3D, true);
 
     return Torus3D;
