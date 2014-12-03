8b092de4487ffdc17ffaf49122f50cfcdda62077
Neonunux <regis.leloup@colombbus.org>
2014-12-03 15:50:09 +0100

    Translation methods for Box3D

--- js/tangara/objects/3dbox/Box3D.js
+++ js/tangara/objects/3dbox/Box3D.js
@@ -1,4 +1,4 @@
-define(['jquery', 'babylon', 'TEnvironment', 'TObject3D'], function($, babylon, TEnvironment, TObject3D) {
+define(['jquery', 'TEnvironment', 'TObject3D'], function($, TEnvironment, TObject3D) {
     /**
      * 
      * @returns Box3D
@@ -14,8 +14,32 @@ define(['jquery', 'babylon', 'TEnvironment', 'TObject3D'], function($, babylon, 
     Box3D.prototype._setSpace = function(scene3d) {
         TObject3D.prototype._setSpace.call(this, scene3d);
         //scene = scene3d;
-        //object3d = BABYLON.Mesh.CreateBox("box1", 2, scene);
+        this.object3d = BABYLON.Mesh.CreateBox("box1", 2, this.scene);
         //object3d.position.y = 1;
+/*        
+Point3d p1 = new Point3d(-sizeX/2, -sizeY/2, sizeZ/2);
+Point3d p2 = new Point3d(sizeX/2, -sizeY/2, sizeZ/2);
+Point3d p3 = new Point3d(sizeX/2, sizeY/2, sizeZ/2);
+Point3d p4 = new Point3d(-sizeX/2, sizeY/2, sizeZ/2);
+Point3d p5 = new Point3d(-sizeX/2, -sizeY/2, -sizeZ/2);
+Point3d p6 = new Point3d(sizeX/2, -sizeY/2, -sizeZ/2);
+Point3d p7 = new Point3d(sizeX/2, sizeY/2, -sizeZ/2);
+Point3d p8 = new Point3d(-sizeX/2, sizeY/2, -sizeZ/2);
+front = new Quadrilateral3D(p1, p2, p3, p4);
+back = new Quadrilateral3D(p5, p8, p7, p6);
+bottom = new Quadrilateral3D(p1, p5, p6, p2);
+left = new Quadrilateral3D(p1, p4, p8, p5);
+right = new Quadrilateral3D(p2, p6, p7, p3);
+top = new Quadrilateral3D(p3, p7, p8, p4);
+addObject(bottom);
+addObject(left);
+addObject(front);
+addObject(right);
+addObject(back);
+addObject(top);
+*/
+        
+        
     };
 
     Box3D.prototype._setBackColor = function(color, transparency) {
