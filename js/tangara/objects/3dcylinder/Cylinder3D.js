687454bf3a46fae21fc601e74f61b0d7b5a687b4
Neonunux <regis.leloup@colombbus.org>
2014-12-03 15:53:35 +0100

    Cylindre3D , Vector3D and TObject3D

--- js/tangara/objects/3dcylinder/Cylinder3D.js
+++ js/tangara/objects/3dcylinder/Cylinder3D.js
@@ -17,7 +17,7 @@ define(['jquery', 'babylon', 'TEnvironment', 'TObject3D'], function($, babylon, 
 
     Cylinder3D.prototype._setSpace = function(scene3d) {
         TObject3D.prototype._setSpace.call(this, scene3d);
-        this.object3d = this.getMesh().CreateCylinder(this.getName(), 3, 1, 1, 16, this.scene);
+        this.object3d = this.getMesh().CreateCylinder("cylindre1", 3, 1, 1, 16, this.scene);
         this.object3d.position.y = 1;
     };
 
@@ -32,15 +32,27 @@ define(['jquery', 'babylon', 'TEnvironment', 'TObject3D'], function($, babylon, 
 });
 
 /* 
- e=new Espace3D()
- c=new Cylindre3D()
- e.addObject(c)
- c.cacher()
- 
- e=new Espace3D()
- c=new Cylindre3D()
- d=new Cylindre3D()
- e.addObject(c)
- tangara.écrire("nom " + d.getName())
+e=new Espace3D()
+c=new Cylindre3D()
+e.addObject(c)
+c.cacher()
+
+
+e=new Espace3D()
+c=new Cylindre3D()
+d=new Cylindre3D()
+e.addObject(c)
+tangara.écrire("nom " + d.getName())
+
+
+e=new Espace3D()
+c=new Cylindre3D()
+d=new Cylindre3D()
+e.addObject(c)
+e.addObject(d)
+tangara.écrire("nom " + c.getName())
+tangara.écrire("nom " + d.getName())
+* @param Space3D space
+     
  
  */
--- js/tangara/objects/3dvector/Vector3D.js
+++ js/tangara/objects/3dvector/Vector3D.js
@@ -5,6 +5,7 @@ define(['jquery', 'babylon', 'TEnvironment', 'TObject'], function($, babylon, TE
      */
     var Vector3D = function() {
         TObject.call(this);
+        return BABYLON.Vector3;
     };
     var object3d;
     var name;
@@ -12,18 +13,20 @@ define(['jquery', 'babylon', 'TEnvironment', 'TObject'], function($, babylon, TE
     Vector3D.prototype = Object.create(TObject.prototype);
     Vector3D.prototype.constructor = Vector3D;
     Vector3D.prototype.className = "Vector3D";
-    Vector3D.prototype.mesh = BABYLON.Mesh;
-    Vector3D.prototype.instances;
+//    Vector3D.prototype.instances;
 
     Vector3D.prototype._setSpace = function(scene3d) {
         this.scene = scene3d;
     };
 
     Vector3D.prototype._getX = function() {
+        return;
     };
     Vector3D.prototype._getY = function() {
+        return;
     };
     Vector3D.prototype._getZ = function() {
+        return;
     };
     Vector3D.prototype._setComponents = function(x, y, z) {
     };
