615dcbd2b1541c1b663953c9c382657d5167e876
Régis LELOUP <regis.leloup@colombbus.org>
2015-04-21 21:56:02 +0200

    Completer improved to manage these cases: r=new Animation();
    direction = new CommandesClavier()
    direction.ajouterCommande("touche", "r.
    direction.ajouterCommande("touche", "r."
    direction.ajouterCommande("touche", "r.")
    
    direction.ajouterCommande("touche", 'r.
    direction.ajouterCommande("touche", 'r.'
    direction.ajouterCommande("touche", 'r.')

--- js/tangara/TEditor.js
+++ js/tangara/TEditor.js
@@ -1,4 +1,4 @@
-define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 'ace/autocomplete', 'TProgram', 'TEnvironment', 'TLink', 'TUI', 'TUtils'], function($,ace, ace_edit_session, ace_range, ace_undo_manager, ace_autocomplete, TProgram, TEnvironment, TLink, TUI, TUtils) {
+define(['jquery', 'ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 'ace/autocomplete', 'TProgram', 'TEnvironment', 'TLink', 'TUI', 'TUtils'], function ($, ace, ace_edit_session, ace_range, ace_undo_manager, ace_autocomplete, TProgram, TEnvironment, TLink, TUI, TUtils) {
 
     function TEditor() {
         var domEditor = document.createElement("div");
@@ -14,32 +14,32 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
         var disabled = false;
         var disabledSession = new AceEditSession('');
         var disabledMessage = document.createElement("div");
-        disabledMessage.id =  "disabled-message";
+        disabledMessage.id = "disabled-message";
         var disabledP = document.createElement("p");
         var disabledText = TEnvironment.getMessage("editor-disabled");
         disabledP.appendChild(document.createTextNode(disabledText));
         disabledMessage.appendChild(disabledP);
-        
+
         var popupTriggered = false;
         var popupTimeout;
         var triggerPopup = false;
         var editionEnabled = false;
-        
-        this.getElement = function() {
+
+        this.getElement = function () {
             return domEditor;
         };
-        
-        this.displayed = function() {
-            aceEditor = ace.edit(domEditor.id);            
+
+        this.displayed = function () {
+            aceEditor = ace.edit(domEditor.id);
             aceEditor.setShowPrintMargin(false);
             //aceEditor.renderer.setShowGutter(false);
             aceEditor.setFontSize("20px");
             aceEditor.setHighlightActiveLine(false);
             aceEditor.setBehavioursEnabled(false);
-            
+
             var self = this;
-            aceEditor.on('input', function() {
-                if (!program.isModified()&&editionEnabled) {
+            aceEditor.on('input', function () {
+                if (!program.isModified() && editionEnabled) {
                     program.setModified(true);
                     TUI.updateSidebarPrograms();
                 }
@@ -47,7 +47,7 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
                 self.removeError();
                 if (triggerPopup) {
                     triggerPopup = false;
-                    popupTimeout = setTimeout(function() {
+                    popupTimeout = setTimeout(function () {
                         popupTriggered = false;
                         AceAutocomplete.startCommand.exec(aceEditor);
                     }, 800);
@@ -60,7 +60,7 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
             aceEditor.commands.addCommand({
                 name: "save",
                 bindKey: {win: "Ctrl-S", mac: "Command-S"},
-                exec: function(arg) {
+                exec: function (arg) {
                     if (editionEnabled) {
                         TUI.saveProgram();
                     }
@@ -68,79 +68,79 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
             });
 
             aceEditor.completers = [editorCompleter];
-            
+
             this.enableMethodHelper();
-            
+
             // disable editor, waiting for a program to edit
             this.disable();
         };
-        
-        this.show = function() {
+
+        this.show = function () {
             $(domEditor).show();
             aceEditor.focus();
         };
-        
-        this.hide = function() {
+
+        this.hide = function () {
             $(domEditor).hide();
         };
-        
-        this.getValue = function() {
+
+        this.getValue = function () {
             return aceEditor.getSession().getValue();
         };
-        
-        this.getStatements = function() {
+
+        this.getStatements = function () {
             this.updateProgram();
             return program.getStatements();
         };
-        
-        this.updateProgram = function() {
+
+        this.updateProgram = function () {
             if (codeChanged) {
                 program.setCode(this.getValue());
                 codeChanged = false;
             }
-       };
-        
-        this.getProgram = function() {
+        };
+
+        this.getProgram = function () {
             return program;
         };
 
-        this.setProgram = function(value) {
+        this.setProgram = function (value) {
             program = value;
             codeChanged = true;
         };
 
-        
-        this.getProgramName = function() {
+
+        this.getProgramName = function () {
             return program.getName();
         };
-        
-        this.setSession = function(session) {
-             if (disabled) {
+
+        this.setSession = function (session) {
+            if (disabled) {
                 aceEditor.setReadOnly(false);
                 aceEditor.renderer.setShowGutter(true);
                 $(domEditor).removeClass('editor-disabled');
                 domEditor.removeChild(disabledMessage);
                 disabled = false;
-                TUI.setEditionEnabled(true);                
-             }
+                TUI.setEditionEnabled(true);
+            }
             aceEditor.setSession(session);
         };
-        
-        this.getSession = function() {
+
+        this.getSession = function () {
             return aceEditor.getSession();
         };
 
-        this.reset = function() {
+        this.reset = function () {
             var undo = aceEditor.getSession().getUndoManager();
             undo.reset();
             codeChanged = false;
         };
-        
-        this.giveFocus = function() {
+
+        this.giveFocus = function () {
             aceEditor.focus();
         };
 
-        this.disable = function() {
+        this.disable = function () {
             aceEditor.setSession(disabledSession);
             aceEditor.setReadOnly(true);
             aceEditor.renderer.setShowGutter(false);
@@ -150,28 +150,30 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
             disabled = true;
         };
 
-        this.removeError = function() {
+        this.removeError = function () {
             if (errorMarker !== null) {
                 aceEditor.getSession().removeMarker(errorMarker);
                 errorMarker = null;
             }
         };
-        
-        this.setError = function(lines) {
+
+        this.setError = function (lines) {
             this.removeError();
             if (lines.length > 1) {
-                var range = new AceRange(lines[0]-1,0,lines[1]-1,100);
+                var range = new AceRange(lines[0] - 1, 0, lines[1] - 1, 100);
                 errorMarker = aceEditor.getSession().addMarker(range, 'tangara_error', 'line', true);
             } else if (lines.length > 0) {
-                var range = new AceRange(lines[0]-1,0,lines[0]-1,100);
+                var range = new AceRange(lines[0] - 1, 0, lines[0] - 1, 100);
                 errorMarker = aceEditor.getSession().addMarker(range, 'tangara_error', 'line', true);
             }
-            aceEditor.navigateTo(lines[0]-1, 0);
+            aceEditor.navigateTo(lines[0] - 1, 0);
             // In a timer, because otherwise does not seem to work when editor mode has just been activated
-            setTimeout(function() { aceEditor.scrollToLine(lines[0]-1, true, true, null); }, 100);
+            setTimeout(function () {
+                aceEditor.scrollToLine(lines[0] - 1, true, true, null);
+            }, 100);
         };
-        
-        this.createSession = function(program) {
+
+        this.createSession = function (program) {
             var session = new AceEditSession(program.getCode());
             session.setMode("ace/mode/javascript");
             session.setUndoManager(new AceUndoManager());
@@ -179,25 +181,25 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
             session.setUseWorker(false);
             return session;
         };
-        
-        this.enableMethodHelper = function() {
+
+        this.enableMethodHelper = function () {
             aceEditor.commands.addCommand(dotCommand);
             aceEditor.commands.addCommand(backspaceCommand);
             aceEditor.commands.addCommand(AceAutocomplete.startCommand);
         };
 
-        this.disableMethodHelper = function() {
+        this.disableMethodHelper = function () {
             aceEditor.commands.removeCommand(dotCommand);
             aceEditor.commands.removeCommand(backspaceCommand);
             aceEditor.commands.removeCommand(AceAutocomplete.startCommand);
         };
-        
-        this.setEditionEnabled = function(value) {
+
+        this.setEditionEnabled = function (value) {
             editionEnabled = value;
         };
 
         var editorCompleter = {
-            getCompletions: function(editor, session, pos, prefix, callback) {
+            getCompletions: function (editor, session, pos, prefix, callback) {
                 pos.column--;
                 var token = session.getTokenAt(pos.row, pos.column);
                 var endToken = "(";
@@ -211,19 +213,19 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
 
                 // TODO: see if we can handle this situation in js
                 /*if (token.type === "rparen") {
-                    // Right parenthesis: try to find actual identifier
-                    while (index >0 & token.type !== "identifier") {
-                        index--;
-                        token = tokens[index];
-                    }
-                    endToken = "[";
-                }*/
-
-                if (token.type !== "identifier" &&  token.type !== "text" && token.type !== "string") {
+                 // Right parenthesis: try to find actual identifier
+                 while (index >0 & token.type !== "identifier") {
+                 index--;
+                 token = tokens[index];
+                 }
+                 endToken = "[";
+                 }*/
+                if (token.type !== "identifier" && token.type !== "text" && token.type !== "string") {
                     return false;
                 }
 
                 var name = token.value.trim();
+                console.debug("name " + name);
 
                 for (var i = index - 1; i >= 0; i--) {
                     token = tokens[i];
@@ -236,32 +238,46 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
                     }
                     name = part + name;
                 }
-                if (token.type === "text") {
-                    // non whole writed command:
-                    //name = name.slice(1, name.length - 1); // "r. -> r
-                    name = name.slice(1, name.length - 2); // "r." -> r
-                }
-
                 if (name.length === 0) {
                     return false;
                 }
 
-                var range = new AceRange(0,0,pos.row, pos.column);
+                var lastcar = name.slice(name.length - 1, name.length);
+                var lastlastcar = name.slice(name.length - 2, name.length - 1);
+                var firstcar = name.slice(0, 1);
+
+                if (token.type === "text") {
+                    // Remove the first
+                    if (firstcar === '"' || firstcar === "'") {
+                        name = name.slice(1, name.length); // "r. -> r.
+                    }
+                    if (lastcar === '.') {
+                        name = name.slice(0, name.length - 1); // "r. -> r
+                    }
+                    if (lastlastcar === '.' && (lastcar === '"' || firstcar === "'")) {
+                        name = name.slice(0, name.length - 2); // "r" -> r
+                    }
+                    if (lastlastcar !== '.' && (lastcar === '"' || firstcar === "'")) {
+                        name = name.slice(0, name.length - 1); // "r") -> r
+                    }
+                }
+
+                var range = new AceRange(0, 0, pos.row, pos.column);
                 var valueBefore = session.getDocument().getTextRange(range);
                 // Since regex do not support unicode...
                 var unicodeName = TUtils.toUnicode(name);
-                var regex = new RegExp("(?:^|\\s)"+unicodeName+"\\s*=\\s*new\\s*([\\S^\\"+endToken+"]*)\\s*\\"+endToken);
+                var regex = new RegExp("(?:^|\\s)" + unicodeName + "\\s*=\\s*new\\s*([\\S^\\" + endToken + "]*)\\s*\\" + endToken);
 
                 var result = regex.exec(valueBefore);
 
                 var completions = [];
 
-                if (result !== null && result.length>0) {
+                if (result !== null && result.length > 0) {
                     var className = result[1];
                     var methods = TEnvironment.getTranslatedClassMethods(className);
                     var methodNames = Object.keys(methods);
                     methodNames = TUtils.sortArray(methodNames);
-                    for (var i=0;i<methodNames.length;i++) {
+                    for (var i = 0; i < methodNames.length; i++) {
                         completions.push({
                             caption: methodNames[i],
                             value: methods[methodNames[i]]
@@ -271,23 +287,24 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
                 callback(null, completions);
             }
         };
-        
+
         var dotCommand = {
             name: "methodHelper",
-            bindKey: {win: '.',  mac: '.'},
-            exec: function(editor) {
-                triggerPopup = true;;
+            bindKey: {win: '.', mac: '.'},
+            exec: function (editor) {
+                triggerPopup = true;
+                ;
                 return false; // let default event perform
             },
             readOnly: true // false if this command should not apply in readOnly mode
         };
-        
+
         var backspaceCommand = {
             name: "methodHelper2",
-            bindKey: {win: 'Backspace',  mac: 'Backspace'},
-            exec: function(editor) {
+            bindKey: {win: 'Backspace', mac: 'Backspace'},
+            exec: function (editor) {
                 var cursor = editor.selection.getCursor();
-                var token = editor.getSession().getTokenAt(cursor.row, cursor.column-1);
+                var token = editor.getSession().getTokenAt(cursor.row, cursor.column - 1);
                 if (token !== null && token.type === "punctuation.operator" && token.value === ".") {
                     triggerPopup = true;
                 }
@@ -295,8 +312,8 @@ define(['jquery','ace/ace', 'ace/edit_session', 'ace/range', 'ace/undomanager', 
             },
             readOnly: true // false if this command should not apply in readOnly mode
         };
-        
-    };
-    
+    }
+    ;
+
     return TEditor;
 });
