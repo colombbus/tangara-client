b7475492e57495bf308b62081077077c2e5a0807
bonpra <benoit@192.168.0.16>
2015-05-10 18:09:48 +0200

    Added SynchronousManager to handle synchronous commands

---
+++ js/tangara/SynchronousManager.js
@@ -0,0 +1,23 @@
+define(['TRuntime'], function(TRuntime) {
+    var SynchronousManager = function() {
+        this.running = false;
+    };
+    
+    SynchronousManager.prototype.begin = function() {
+        TRuntime.suspend();
+        this.running = true;
+    };
+    
+    SynchronousManager.prototype.end = function() {
+        TRuntime.resume();
+        this.running = false;
+    };
+    
+    SynchronousManager.prototype.isRunning = function() {
+        return this.running;
+    };
+
+    return SynchronousManager;
+});
+
+
