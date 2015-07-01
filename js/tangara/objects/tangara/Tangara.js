e378b5f878993dc54c94c75640da3f4187b0a6a2
Neonunux <regis.leloup@colombbus.org>
2015-07-01 19:38:24 +0200

    alert and write method updated to support number entries As : tangara._write(5) tangara._write(5.5)

--- js/tangara/objects/tangara/Tangara.js
+++ js/tangara/objects/tangara/Tangara.js
@@ -1,5 +1,5 @@
-define(['jquery', 'TUI', 'TEnvironment', 'TRuntime', 'TUtils', 'TObject', 'TLink'], function($, TUI, TEnvironment, TRuntime, TUtils, TObject, TLink) {
-    var Tangara = function() {
+define(['jquery', 'TUI', 'TEnvironment', 'TRuntime', 'TUtils', 'TObject', 'TLink'], function ($, TUI, TEnvironment, TRuntime, TUtils, TObject, TLink) {
+    var Tangara = function () {
         // Do not call parent constructor, as we don't want this object to be erased when clearing the
         // Runtime
     };
@@ -8,41 +8,52 @@ define(['jquery', 'TUI', 'TEnvironment', 'TRuntime', 'TUtils', 'TObject', 'TLink
     Tangara.prototype.constructor = Tangara;
     Tangara.prototype.className = "Tangara";
 
-    Tangara.prototype._write = function(value) {
-        value = TUtils.getString(value);
+    Tangara.prototype._write = function (value) {
+        if (TUtils.checkInteger(value)) {
+            value = value.toString();
+            //tangara.écrire(5);
+        } else {
+            value = TUtils.getString(value);
+        }
+
         TUI.addLogMessage(value);
     };
 
-    Tangara.prototype._alert = function(value) {
-        value = TUtils.getString(value);
+    Tangara.prototype._alert = function (value) {
+        if (TUtils.checkInteger(value)) {
+            value = value.toString();
+            //tangara.écrire(5);
+        } else {
+            value = TUtils.getString(value);
+        }
         window.alert(value);
     };
 
-    Tangara.prototype._loadScript = function(name) {
+    Tangara.prototype._loadScript = function (name) {
         name = TUtils.getString(name);
         var statements = TLink.getProgramStatements(name);
         TRuntime.setCurrentProgramName(name);
         TRuntime.executeStatements(statements);
     };
 
-    Tangara.prototype._init = function() {
+    Tangara.prototype._init = function () {
         TRuntime.clear();
     };
-    
-    Tangara.prototype._clearScreen = function() {
+
+    Tangara.prototype._clearScreen = function () {
         TRuntime.clearGraphics();
     };
-    
-    Tangara.prototype._pause = function() {
+
+    Tangara.prototype._pause = function () {
         TRuntime.stop();
     };
 
 
-    Tangara.prototype._unpause = function() {
+    Tangara.prototype._unpause = function () {
         TRuntime.start();
     };
 
-    Tangara.prototype._ask = function(text) {
+    Tangara.prototype._ask = function (text) {
         var answer = window.prompt(text);
         if (answer === null || answer.length === 0)
             return false;
