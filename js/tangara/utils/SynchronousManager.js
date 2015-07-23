4cc5b562d232cae415fb65196445aaeeb834210d
LiaOkami <pierrick.garcia@hotmail.fr>
2015-07-23 17:21:08 +0200

    SynchronousManager documentation added

--- js/tangara/utils/SynchronousManager.js
+++ js/tangara/utils/SynchronousManager.js
@@ -1,23 +1,36 @@
 define(['TRuntime'], function(TRuntime) {
+    /**
+     * SynchronousManager runs the synchronicity between objects in TRunTime
+     * @class
+     * @returns {SynchronousManager}
+     */
     var SynchronousManager = function() {
         this.running = false;
     };
 
+    /**
+     * Suspend the execution of the interpreter and set running to true
+     */
     SynchronousManager.prototype.begin = function() {
         TRuntime.suspend();
         this.running = true;
     };
 
+    /**
+     * Resume the execution of the interpreter and set running to false
+     */
     SynchronousManager.prototype.end = function() {
         TRuntime.resume();
         this.running = false;
     };
 
+    /**
+     * Check the value of running.
+     * @returns {Boolean}
+     */
     SynchronousManager.prototype.isRunning = function() {
         return this.running;
     };
 
     return SynchronousManager;
 });
-
-
