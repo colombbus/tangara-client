9a36a0b274521ae11408a5f34524a29010470359
Thomas_Fr <thomastel94@hotmail.fr>
2015-08-17 14:56:00 +0200

    Display an error when the score isn't between 0 and 1.

--- js/tangara/objects/teacher/Teacher.js
+++ js/tangara/objects/teacher/Teacher.js
@@ -136,7 +136,9 @@ define(['jquery', 'TEnvironment', 'TRuntime', 'TUtils', 'SynchronousManager', 'T
             score = value;
         }
         else {
-            window.console.log("Score must be between 0 and 1.")
+            //display the problem, a bit dirty
+            message = "Score must be between 0 and 1.";
+            window.console.log(message);
         }
     };
     
