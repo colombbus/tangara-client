f55e8128a53e77194901fb1fc0e5ef6fce802f5b
bonpra <benoit.prady@colombbus.org>
2015-09-25 14:14:38 +0200

    Added log messages for debug

--- js/tangara/env/Grader.js
+++ js/tangara/env/Grader.js
@@ -18,6 +18,8 @@ define(['platform-pr', 'json'], function() {
         this.gradeTask = function (strAnswer, token, callback) {
             var answer = JSON.parse(strAnswer);
             var score = answer.score * platform.getTaskParams().maxScore;
+            window.console.log("SENDING SCORE: "+answer.score);
+            window.console.log("MAX SCORE: "+platform.getTaskParams().maxScore);
             var message = answer.message;
             callback(score, message);
         };           
