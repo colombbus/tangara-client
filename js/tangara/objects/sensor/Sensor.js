0c390d0d163dd4a3fd414c7d68118e8c3ba9f488
LiaOkami <pierrick.garcia@hotmail.fr>
2015-09-30 12:02:23 +0200

    Sensor : Several methods added.

--- js/tangara/objects/sensor/Sensor.js
+++ js/tangara/objects/sensor/Sensor.js
@@ -17,7 +17,11 @@ define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager',
         init: function (props, defaultProps) {
             this._super(TUtils.extend({
                 initialized: true,
-                hidden: true
+                hidden: true,
+                color: "#FF0000",
+                width: 1,
+                fill: true,
+                fillColor: "#800000"
             }, props), defaultProps);
             this.watchCollisions(true);
         },
@@ -38,10 +42,25 @@ define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager',
             ctx.lineTo(p.w, p.h);
             ctx.lineTo(0, p.h);
             ctx.closePath();
-            ctx.strokeStyle = "#FF0000";
+            ctx.strokeStyle = p.color;
+            ctx.lineWidth = p.width;
             ctx.stroke();
-            ctx.fillStyle = "#800000"
-            ctx.fill();
+            if (this.p.fill) {
+                ctx.fillStyle = p.fillColor;
+                ctx.fill();
+            }
+        },
+        color: function(red, green, blue) {
+           this.p.color = TUtils.rgbToHex(TUtils.getColor(red, green, blue));
+        },
+        width: function(value) {
+            this.p.width = value;
+        },
+        fill: function(value) {
+            this.p.fill = value;
+        },
+        fillColor: function(red, green, blue) {
+           this.p.fillColor = TUtils.rgbToHex(TUtils.getColor(red, green, blue));
         }
     });
 
@@ -56,5 +75,50 @@ define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager',
         this.gObject.setSize(w, h);
     };
 
+    /**
+     * Change the color of the sensor.</br>
+     * Default value : red | [255, 0, 0]
+     * @param {String|Number} red
+     * @param {Number} green
+     * @param {Number} blue
+     */
+    Sensor.prototype._color = function(red, green, blue) {
+        this.gObject.color(red, green, blue);
+    };
+    
+    /**
+     * Set the width of the stroke.
+     * Default value : 1.
+     * @param {Number} value
+     */
+    Sensor.prototype._width = function(value) {
+        if (typeof value !== 'undefined') {
+            value = TUtils.getInteger(value);
+            this.gObject.width(value);
+        }
+    };
+        
+    /**
+     * Enable or disable the fill of the sensor.
+     * Default value : False. 
+     * @param {Boolean} value
+     */
+    Sensor.prototype._fill = function(value) {
+        if (typeof value !== 'undefined') {
+            this.gObject.fill(value);
+        }
+    };
+    
+    /**
+     * Change the color of the sensor's fill.
+     * Default value : marron | [128, 0, 0]
+     * @param {String|Number} red
+     * @param {Number} green
+     * @param {Number} blue
+     */
+    Sensor.prototype._fillColor = function(red, green, blue) {
+        this.gObject.fillColor(red, green, blue);
+    };
+    
     return Sensor;
 });
