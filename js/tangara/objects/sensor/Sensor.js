5a39ebd241e579099682c86accd5c68d1cce11e2
LiaOkami <pierrick.garcia@hotmail.fr>
2015-09-30 11:38:38 +0200

    Sensor can now be seen.

--- js/tangara/objects/sensor/Sensor.js
+++ js/tangara/objects/sensor/Sensor.js
@@ -1,9 +1,9 @@
-define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager', 'objects/sprite/Sprite'], function($, TEnvironment, TUtils, CommandManager, ResourceManager, Sprite) {
+define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager', 'objects/sprite/Sprite'], function ($, TEnvironment, TUtils, CommandManager, ResourceManager, Sprite) {
     /**
      * Defines Sensor, inherited from Sprite.
      * @exports Sensor
      */
-    var Sensor = function() {
+    var Sensor = function () {
         Sprite.call(this);
     };
 
@@ -14,14 +14,15 @@ define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager',
     var graphics = Sensor.prototype.graphics;
 
     Sensor.prototype.gClass = graphics.addClass("TSprite", "TSensor", {
-        init: function(props, defaultProps) {
+        init: function (props, defaultProps) {
             this._super(TUtils.extend({
-                initialized: true
+                initialized: true,
+                hidden: true
             }, props), defaultProps);
             this.watchCollisions(true);
         },
-        setSize: function(w, h) {
-            this.perform(function(w, h) {
+        setSize: function (w, h) {
+            this.perform(function (w, h) {
                 this.p.w = w;
                 this.p.h = h;
                 graphics.objectResized(this);
@@ -29,14 +30,27 @@ define(['jquery', 'TEnvironment', 'TUtils', 'CommandManager', 'ResourceManager',
                 this.p.y += h / 2;
             }, [w, h]);
         },
+        draw: function (ctx) {
+            var p = this.p;
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(p.w, 0);
+            ctx.lineTo(p.w, p.h);
+            ctx.lineTo(0, p.h);
+            ctx.closePath();
+            ctx.strokeStyle = "#FF0000";
+            ctx.stroke();
+            ctx.fillStyle = "#800000"
+            ctx.fill();
+        }
     });
-    
+
     /**
      * Set Sensor's width and height.
      * @param {Number} w
      * @param {Number} h
      */
-    Sensor.prototype._setSize = function(w, h) {
+    Sensor.prototype._setSize = function (w, h) {
         h = TUtils.getInteger(w);
         w = TUtils.getInteger(h);
         this.gObject.setSize(w, h);
