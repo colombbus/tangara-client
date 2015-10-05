6a18172b8c360418e059b941b401ea2d667308a4
LiaOkami <pierrick.garcia@hotmail.fr>
2015-10-05 12:17:45 +0200

    Platform : _build* methods added.

--- js/tangara/objects/platform/Platform.js
+++ js/tangara/objects/platform/Platform.js
@@ -23,7 +23,11 @@ define(['jquery', 'TGraphicalObject', 'TUtils', 'ResourceManager', 'TEnvironment
     Platform.prototype = Object.create(TGraphicalObject.prototype);
     Platform.prototype.constructor = Platform;
     Platform.prototype.className = "Platform";
-
+    Platform.BRICK = 0x01;
+    Platform.DOOR = 0x02;
+    Platform.EXIT = 0x03;
+    Platform.WALL = 0x04;
+    
     var graphics = Platform.prototype.graphics;
     
     
@@ -211,7 +215,26 @@ define(['jquery', 'TGraphicalObject', 'TUtils', 'ResourceManager', 'TEnvironment
 	      if(this.blocks[blockY]  && this.blocks[blockY][blockX]) {
 	        ctx.drawImage(this.blocks[blockY][blockX],startX,startY);
 	      }
-	    }       
+	},
+        addTile: function(number, x, y) {
+            var p = this.p;
+            if (typeof x === 'undefined') {
+                x = p.gridX;
+                y = p.gridY;
+            } else {
+                x = TUtils.getInteger(x);
+                y = TUtils.getInteger(y);
+            }
+            if (x >= p.nbColumns) {
+                this.addColumns(x);
+            }
+            if (y >= p.nbRows) {
+                this.addRows(y);
+            }
+            if (p.platform[y][x] === 0)
+                p.tiles += 1;
+            p.platform[y][x] = number;
+        }
     });
     
     /**
@@ -371,6 +394,47 @@ define(['jquery', 'TGraphicalObject', 'TUtils', 'ResourceManager', 'TEnvironment
             throw new Error(this.getMessage("tile number incorrect", number));
     	}
     	this.gObject.setTile(x,y,number);
+    };
+      
+    /*
+     * Put a brick at given location
+     * If no location given, use current location
+     * @param {Integer} x
+     * @param {Integer} y
+     */
+    Platform.prototype._buildBrick = function(x,y) {
+        this.gObject.addTile(Platform.BRICK,x,y);
+    };
+    
+
+    /*
+     * Build a door at current location 
+     * If no location given, use current location
+     * @param {Integer} x
+     * @param {Integer} y
+     */
+    Platform.prototype._buildDoor = function(x,y) {
+        this.gObject.addTile(Platform.DOOR,x,y);
+    };
+
+    /*
+     * Build an exit at current location 
+     * If no location given, use current location
+     * @param {Integer} x
+     * @param {Integer} y
+     */
+    Platform.prototype._buildExit = function(x,y) {
+        this.gObject.addTile(Platform.EXIT,x,y);
+    };
+    
+    /*
+     * Build an wall at current location 
+     * If no location given, use current location
+     * @param {Integer} x
+     * @param {Integer} y
+     */
+    Platform.prototype._buildWall = function(x,y) {
+        this.gObject.addTile(Platform.WALL,x,y);
     };
     
     /**
