83b48661a8d6628a36fd965cf4e66a1f7f253ad4
bonpra <benoit.prady@colombbus.org>
2015-10-06 19:37:49 +0200

    Now only use Platform to display tiles

--- js/tangara/objects/builder/Builder.js
+++ js/tangara/objects/builder/Builder.js
@@ -5,7 +5,7 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
      * @exports Builder
      */
     var Builder = function() {
-        Robot.call(this, "builder");
+        Robot.call(this, "builder", false);
         this.synchronousManager = new SynchronousManager();
         this.gObject.synchronousManager = this.synchronousManager;
         var gObject = this.gObject;
@@ -14,6 +14,7 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
         this.platform.addTile("door.png", this.getResource("door.png"));
         this.platform.addTile("exit.png", this.getResource("exit.png"));
         this.platform.addTile("wall.png", this.getResource("wall.png"));
+        this.platform._build();
     };
 
     Builder.prototype = Object.create(Robot.prototype);
@@ -29,121 +30,13 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
     Builder.prototype.gClass = graphics.addClass("TRobot", "TBuilder", {
         init: function(props, defaultProps) {
             this._super(TUtils.extend({
-                platform: [],
-                nbRows: 0,
-                nbColumns: 0,
-                tiles: 0
             }, props), defaultProps);
         },
-        addRow: function() {
-            var p = this.p;
-            p.platform[p.nbRows] = [];
-            for (var j=0; j< p.nbColumns; j++) {
-                p.platform[p.nbRows][j] = 0;
-            }
-            p.nbRows++;
+        getGridX:function() {
+            return this.p.gridX;
         },
-        addColumn: function() {
-            var p = this.p;
-            for (var i=0; i< p.nbRows; i++) {
-                p.platform[i][p.nbColumns] = 0;
-            }
-            p.nbColumns++;
-        },
-        addRows: function(y) {
-            var p = this.p;
-            if (typeof y === 'undefined') {
-                y = p.gridY;
-            }
-            for (var i= p.nbRows ; i<=y ; i++) {
-                this.addRow();
-            }
-        },
-        addColumns: function(x) {
-            var p = this.p;
-            if (typeof x === 'undefined') {
-                x = p.gridX;
-            }
-            for (var j= p.nbColumns ; j<=x ; j++) {
-                this.addColumn();
-            }
-        },
-        addTile: function(number, x, y) {
-            var p = this.p;
-            if (typeof x === 'undefined') {
-                x = p.gridX;
-                y = p.gridY;
-            } else {
-                x = TUtils.getInteger(x);
-                y = TUtils.getInteger(y);
-            }
-            if (x >= p.nbColumns) {
-                this.addColumns(x);
-            }
-            if (y >= p.nbRows) {
-                this.addRows(y);
-            }
-            if (p.platform[y][x] === 0)
-                p.tiles += 1;
-            p.platform[y][x] = number;
-        },
-        draw: function(ctx) {
-            var p = this.p;
-            for (var i = 0; i < p.nbRows ; i++)
-            {
-                for (var j = 0 ; j < p.nbColumns ; j++)
-                {
-                    if (p.platform[i][j]) {
-                        ctx.beginPath();
-                        ctx.moveTo(j * p.length - p.x, i * p.length - p.y);
-                        ctx.lineTo((j + 1) * p.length - p.x, i * p.length - p.y);
-                        switch (p.platform[i][j]) {
-                            case Builder.BRICK: 
-                                ctx.lineTo((j + 1) * p.length - p.x, (i + 0.4) * p.length - p.y);
-                                ctx.lineTo(j * p.length - p.x, (i + 0.4) * p.length - p.y);
-                                ctx.closePath();
-                                ctx.strokeStyle = "#000000";
-                                ctx.stroke();
-                                ctx.fillStyle = "#000000";
-                                ctx.fill();
-                                break;
-                            case Builder.DOOR:
-                                ctx.lineTo((j + 1) * p.length - p.x, (i + 1) * p.length - p.y);
-                                ctx.lineTo(j * p.length - p.x, (i + 1) * p.length - p.y);
-                                ctx.closePath();
-                                ctx.strokeStyle = "#8b6f37";
-                                ctx.stroke();
-                                ctx.fillStyle = "#8b6f37";
-                                ctx.fill();
-                                break;
-                            case Builder.EXIT:
-                                ctx.lineTo((j + 1) * p.length - p.x, (i + 1) * p.length - p.y);
-                                ctx.lineTo(j * p.length - p.x, (i + 1) * p.length - p.y);
-                                ctx.closePath();
-                                ctx.strokeStyle = "#00FF00";
-                                ctx.stroke();
-                                ctx.fillStyle = "#00FF00";
-                                ctx.fill();
-                                break;
-                            case Builder.WALL: 
-                                ctx.lineTo((j + 1) * p.length - p.x, (i + 1) * p.length - p.y);
-                                ctx.lineTo(j * p.length - p.x, (i + 1) * p.length - p.y);
-                                ctx.closePath();
-                                ctx.strokeStyle = "#000000";
-                                ctx.stroke();
-                                ctx.fillStyle = "#000000";
-                                ctx.fill();
-                                break;
-                        }
-                    }
-                }
-            }
-
-            if (p.sheet) {
-                this.sheet().draw(ctx, -p.cx, -p.cy, p.frame);
-            } else if (p.asset) {
-                ctx.drawImage(this.resources.getUnchecked(p.asset), -p.cx, -p.cy);
-            }
+        getGridY:function() {
+            return this.p.gridY;
         }
     });
     
@@ -154,7 +47,14 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
      * @param {Integer} y
      */
     Builder.prototype._buildBrick = function(x,y) {
-        this.gObject.addTile(Builder.BRICK,x,y);
+        if (typeof x === 'undefined') {
+            x = this.gObject.getGridX();
+            y = this.gObject.getGridY();
+        } else {
+            x = TUtils.getInteger(x);
+            y = TUtils.getInteger(y);
+        }
+        this.platform._setTile(x,y,Builder.BRICK);
     };
     
 
@@ -165,7 +65,15 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
      * @param {Integer} y
      */
     Builder.prototype._buildDoor = function(x,y) {
-        this.gObject.addTile(Builder.DOOR,x,y);
+        if (typeof x === 'undefined') {
+            x = this.gObject.getGridX();
+            y = this.gObject.getGridY();
+        } else {
+            x = TUtils.getInteger(x);
+            y = TUtils.getInteger(y);
+        }
+        this.platform._setTile(x,y,Builder.DOOR);
+        this.platform.setDoorLocation(x,y);
     };
 
     /*
@@ -175,7 +83,14 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
      * @param {Integer} y
      */
     Builder.prototype._buildExit = function(x,y) {
-        this.gObject.addTile(Builder.EXIT,x,y);
+        if (typeof x === 'undefined') {
+            x = this.gObject.getGridX();
+            y = this.gObject.getGridY();
+        } else {
+            x = TUtils.getInteger(x);
+            y = TUtils.getInteger(y);
+        }
+        this.platform._setTile(x,y,Builder.EXIT);
     };
     
     /*
@@ -185,43 +100,16 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
      * @param {Integer} y
      */
     Builder.prototype._buildWall = function(x,y) {
-        this.gObject.addTile(Builder.WALL,x,y);
-    };
-    
-    /**
-     * Get platform structure
-     * @returns {array}
-     */
-    Builder.prototype._getStructure = function() {
-        var p = this.gObject.p.platform;
-        var p2 = [];
-        
-        if (p.length>0 && p[0].length>0) {
-            var cols = p[0].length;
-            for (var i=0; i<p.length ; i ++) {
-                p2[i] = [];
-                for (var j=0; j<cols; j++) {
-                    if (p[i][j] === Builder.BRICK || p[i][j] === Builder.WALL) {
-                        p2[i][j] = p[i][j];
-                    } else {
-                        p2[i][j] = 0;
-                    }
-                }
-            }
-            return p2;
+        if (typeof x === 'undefined') {
+            x = this.gObject.getGridX();
+            y = this.gObject.getGridY();
         } else {
-            return [[]];
+            x = TUtils.getInteger(x);
+            y = TUtils.getInteger(y);
         }
+        this.platform._setTile(x,y,Builder.WALL);
     };
     
-    /**
-     * Build the Platform inside Builder.
-     */
-    Builder.prototype._buildStructure = function() {
-        this.platform._loadStructure(this._getStructure());
-        this.platform._build();
-    };
-
     /**
      * Set a new tile image. There can be many tiles.
      * Its value in the structure will depend of the moment where it is added :
@@ -237,7 +125,6 @@ define(['jquery', 'TUtils', 'SynchronousManager', 'objects/robot/Robot', 'object
      * @returns {Platform}
      */
     Builder.prototype._getPlatform = function() {
-        this._buildStructure();
         return this.platform;
     };
     
